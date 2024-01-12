97faf5284f9d76fba81108680919c6acb18b7279
DanyaLozhkin <delaynore@gmail.com>
2024-01-12 18:35:01 +0400

    feat: add email as value object and his errors

---
+++ Quiz.Domain/Errors/EmailErrors.cs
@@ -0,0 +1,14 @@
+﻿using Quiz.Domain.Common;
+
+namespace Quiz.Domain.Errors
+{
+    public static class EmailErrors
+    {
+        public static Error NullOrEmptyEmail
+            => Error.Validation("Email.NullOrEmptyValue", "Email can't represent null or empty value");
+
+        public static Error InvalidEmail
+            => Error.Validation("Email.Invalid", "The provided email is not valid");
+    }
+
+}
---
+++ Quiz.Domain/ValueObjects/Email.cs
@@ -0,0 +1,32 @@
+﻿using Quiz.Domain.Common;
+using Quiz.Domain.Primitives;
+using Quiz.Domain.Errors;
+
+namespace Quiz.Domain.ValueObjects
+{
+    public class Email : ValueObject
+    {
+        public string Value { get; }
+
+        private Email(string value)
+        {
+            Value = value;
+        }
+
+        public static Result<Email> Create(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+                return EmailErrors.NullOrEmptyEmail;
+
+            if (!email.Contains('@') || !email.Contains('.'))
+                return EmailErrors.InvalidEmail;
+
+            return new Email(email);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
