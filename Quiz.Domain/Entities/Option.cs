cb4fe8218e348c680b8d869db6c06a14478a1a9e
917sympathies <maraturazbakhtin@yandex.ru>
2024-01-21 21:52:00 +0400

    Question options changed

---
+++ Quiz.Domain/Entities/Option.cs
@@ -0,0 +1,7 @@
+﻿namespace Quiz.Domain.Entities;
+
+public class Option
+{
+    public int Id { get; set; }
+    public string Value { get; set; } = default!;
+}
--- Quiz.Domain/Entities/Question.cs
+++ Quiz.Domain/Entities/Question.cs
@@ -6,6 +6,6 @@ public class Question
     public string Content { get; set; } = string.Empty;
     public string Answer { get; set; } = string.Empty;
     public string Theme { get; set; } = string.Empty;
-    public ICollection<string> Options { get; set; } = new List<string>();
+    public ICollection<Option> Options { get; set; } = new List<Option>();
     public int Value { get; set; }
 }
